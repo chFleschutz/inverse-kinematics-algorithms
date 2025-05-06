00e8ec5c6e1bbf45560b9bb4c559d16b819a18be
Christoph Fleschutz <ch.fleschutz@gmail.com>
2025-05-06 22:44:33 +0200

    Add graphics view

--- demos/ik_gui_demo/IKMainWindow.cpp
+++ demos/ik_gui_demo/IKMainWindow.cpp
@@ -1,10 +1,63 @@
 #include "IKMainWindow.h"
 
+#include <CCD.h>
+#include <FABRIK.h>
+
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+
+TargetItem::TargetItem(float x, float y, float radius, QGraphicsItem* parent)
+	: QGraphicsEllipseItem(x - radius, y - radius, radius * 2, radius * 2, parent)
+{
+	setBrush(Qt::red);
+	setPen(Qt::NoPen);
+	setFlags(ItemIsMovable | ItemSendsScenePositionChanges);
+}
+
+auto TargetItem::itemChange(GraphicsItemChange change, const QVariant& value) -> QVariant
+{
+	if (change == ItemPositionHasChanged)
+	{
+		auto newPos = value.toPointF();
+
+		qDebug() << "Target position changed to:" << newPos;
+	}
+
+	return QGraphicsEllipseItem::itemChange(change, value);
+}
+
+
 IKMainWindow::IKMainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	ui.CCD_radioButton->setChecked(true);
+
+	QGraphicsScene* scene = new QGraphicsScene(this);
+	scene->setSceneRect(-200, -200, 400, 400);
+	ui.graphicsView->setScene(scene);
+
+	scene->addLine(-100, -100, 100, 100, QPen(Qt::blue, 3));
+	scene->addLine(0, 0, 100, 0, QPen(Qt::red, 3));
+
+	m_targetItem = new TargetItem(100, 0, 10);
+	scene->addItem(m_targetItem);
+
+	m_skeleton.addBone(100.0f, radians(30.0f));
+	m_skeleton.addBone(100.0f, radians(30.0f));
 }
 
 IKMainWindow::~IKMainWindow()
-{}
+{
+}
+
+void IKMainWindow::onCCDSelected()
+{
+	m_ikSolver = std::make_unique<CCD>();
+}
+
+void IKMainWindow::onFABRIKSelected()
+{
+	m_ikSolver = std::make_unique<FABRIK>();
+}
--- demos/ik_gui_demo/IKMainWindow.h
+++ demos/ik_gui_demo/IKMainWindow.h
@@ -3,6 +3,24 @@
 #include <QMainWindow>
 #include "ui_IKMainWindow.h"
 
+#include <IKSolver.h>
+#include <Skeleton.h>
+
+#include <QGraphicsEllipseItem>
+
+#include <memory>
+
+
+class TargetItem : public QGraphicsEllipseItem
+{
+public:
+	TargetItem(float x, float y, float radius, QGraphicsItem* parent = nullptr);
+
+protected:
+	auto itemChange(GraphicsItemChange change, const QVariant& value) -> QVariant override;
+};
+
+
 class IKMainWindow : public QMainWindow
 {
 	Q_OBJECT
@@ -11,6 +29,14 @@ public:
 	IKMainWindow(QWidget *parent = nullptr);
 	~IKMainWindow();
 
+public slots:
+	void onCCDSelected();
+	void onFABRIKSelected();
+
 private:
 	Ui::IKMainWindowClass ui;
+
+	std::unique_ptr<IKSolver> m_ikSolver;
+	Skeleton m_skeleton;
+	TargetItem* m_targetItem = nullptr;
 };
